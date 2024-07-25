56aeb72897580049c0a718c14bdb4e44cf67ca2d
VxElA-A <126609781+VxElA-A@users.noreply.github.com>
2024-07-25 19:28:03 +0300

    Homework_1

---
+++ Homework_1/src/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+public class App
+{
+    public static void main( String[] args )
+    {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot parrot1 = context.getBean("Koko", Parrot.class);
+        System.out.println("parrot = " + parrot1.getParrotName());
+
+        Parrot parrot2 = context.getBean("Miki", Parrot.class);
+        System.out.println("parrot = " + parrot2.getParrotName());
+
+        Parrot parrot3 = context.getBean("Kiki", Parrot.class);
+        System.out.println("parrot = " + parrot3.getParrotName());
+
+        String string1 = context.getBean("string1", String.class);
+        System.out.println("string1= " + string1);
+
+        Integer integer1 = context.getBean("integer1", Integer.class);
+        System.out.println("integer1 = " + integer1);
+    }
+}
